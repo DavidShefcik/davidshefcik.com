9d222d9e6099290de84823ae6961dc6190096bdc
David Shefcik <davidshefcik@gmail.com>
2020-07-11 13:22:43 -0500

    Tailwind css.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from "react";
 
+import "./css/tailwind.css";
+
 export default function App(): ReactElement {
   return (
     <div>
-      <p>Hello World!</p>
+      <p className="text-lg">Hello World!</p>
     </div>
   );
 }
