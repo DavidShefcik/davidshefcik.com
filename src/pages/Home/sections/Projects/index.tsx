0ec71b4e11f709cdeb74c6e23b702e719012db10
David Shefcik <davidshefcik@gmail.com>
2020-07-26 03:14:25 -0500

    Remove project carousel code because it won't work nor does it look that good,

--- src/pages/Home/sections/Projects/index.tsx
+++ src/pages/Home/sections/Projects/index.tsx
@@ -1,28 +1,19 @@
 import React, { ReactElement, useEffect, useState } from "react";
-import { MdViewCarousel, MdViewModule } from "react-icons/md";
 import "firebase/firestore";
 
 import ProjectType from "../../../../types/Project";
 
 import HomeSection from "../../../../components/HomeSection";
 
-import Carousel from "./Carousel";
 import Table from "./Table";
 
 import useFirebase from "../../../../store/Firebase";
 
 import useProjects from "../../../../store/home/Projects";
 
-import useProjectsViewType, {
-  ProjectsViewTypes,
-} from "../../../../store/home/ProjectsViewType";
-
 export default function Projects(): ReactElement {
   const projectList = useProjects((state) => state.projects);
   const setProjectList = useProjects((state) => state.setProjects);
-
-  const projectsViewType = useProjectsViewType((state) => state.type);
-  const setProjectsViewType = useProjectsViewType((state) => state.setType);
 
   const firebase = useFirebase((state) => state.firebase);
 
@@ -65,44 +56,9 @@ export default function Projects(): ReactElement {
     setProjectList(projects);
   }, [projects]);
 
-  useEffect(() => {
-    const localStorageViewType = localStorage.getItem("projectviewtype");
-    if (localStorageViewType != null) {
-      setProjectsViewType(localStorageViewType);
-    }
-  }, [setProjectsViewType]);
-
   return (
     <HomeSection title="Projects" status={status}>
-      <>
-        <div className="w-full lg:w-2/3 pb-6 lg:pb-0 flex flex-row items-center justify-center lg:justify-end">
-          <MdViewCarousel
-            size={38}
-            className={`${
-              projectsViewType === ProjectsViewTypes.CAROUSEL
-                ? "text-gray-200"
-                : "text-gray-600 hover:text-gray-400"
-            } cursor-pointer transition ease-in duration-75 mx-2 transition ease-in duration-75`}
-            title="Carousel"
-            onClick={() => setProjectsViewType(ProjectsViewTypes.CAROUSEL)}
-          />
-          <MdViewModule
-            size={38}
-            className={`${
-              projectsViewType === ProjectsViewTypes.TABLE
-                ? "text-gray-200"
-                : "text-gray-600 hover:text-gray-400"
-            } cursor-pointer transition ease-in duration-75 mx-2 transition ease-in duration-75`}
-            title="Carousel"
-            onClick={() => setProjectsViewType(ProjectsViewTypes.TABLE)}
-          />
-        </div>
-        {projectsViewType === ProjectsViewTypes.CAROUSEL ? (
-          <Carousel projects={projects} />
-        ) : (
-          <Table projects={projects} />
-        )}
-      </>
+      <Table projects={projects} />
     </HomeSection>
   );
 }
